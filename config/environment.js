b624eefa3ac562a1c5724e60458f54f949b030e6
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2019-02-28 22:44:47 +0100

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -32,9 +32,9 @@ module.exports = function(environment) {
         label: 'Home',
         route: 'index'
       }, {
-        label: 'Built by Chris Manson',
+        label: 'Camino',
         route: 'page',
-        id: 'chris-manson'
+        id: 'camino'
       }]
     }
   };
